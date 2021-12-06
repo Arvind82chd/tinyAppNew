54578ac4c759b7792f7a761bfc1c5710986a7403
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-06 23:58:33 +0000

    Added the ejs package and set it as engines

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
