5252df417b65358fa7f13a0601f9fd186e6e9665
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-07 10:05:29 +0000

    Added the generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser'); //this converts the request body from buffer into a string
 const app = express();
 const PORT = 5000;
 
 app.set("view engine", "ejs"); //ejs set as view engine
+app.use(bodyParser.urlencoded({extended: true})); //will add data to the req obj under key: body hence input field data will be available in req.body.longURL which canbe stored
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//Functions:
+
+function generateRandomString() {
+  const sampleString = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let shortString = '';
+  for (let i = 0; i < 6; i++) {
+    shortString += sampleString.charAt(Math.floor(Math.random() * 62));
+  } return shortString;
+}
+
+//GET:
 
 // route handler for home page
 app.get("/", (req, res) => {
@@ -37,6 +52,12 @@ app.get('/urls/:shortURL', (req, res) => { //:notation to represent the value of
   res.render('urls_show', templateVars);
 });
 
+
+//POST:
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listen on port ${PORT}!`);
